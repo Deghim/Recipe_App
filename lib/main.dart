6f61caacddac0aee744e8502ed6f1adccd521bc8
Jorge Chavira <jorgechavira4@gmail.com>
2025-03-08 18:16:11 -0600

    Modificacion de tabs y eliminacion de la pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,39 +22,20 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Recipe Book", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        width:
-            MediaQuery.of(context)
-                .size
-                .width, // MediaQuery.of(context)... proporciona datos sobre el tamaño de la pantalla, la orientación, la densidad de píxeles, los márgenes seguros (safe areas), la escala del texto y otros aspectos del diseño.
-        height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
-                ),
-              ),
-              SizedBox(width: 20),
-              Column(
-                children: <Widget>[
-                  Text("Lasagna"),
-                  Text("Jorge U. Chavira"),
-                  Container(height: 1, width: 75, color: Colors.green),
-                ],
-              ),
-            ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Recipe Book", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.green,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [Tab(icon: Icon(CupertinoIcons.home), text: "Home")],
           ),
         ),
+        body: TabBarView(children: <Widget>[HomeScreen()]),
       ),
     );
   }
