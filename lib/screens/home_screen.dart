2413d48272b5ca417243b53e310286ca176b6051
Jorge Chavira <jorgechavira4@gmail.com>
2025-03-08 18:16:34 -0600

    Creacion de la pantalla del listado de recetas.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width:
+            MediaQuery.of(context)
+                .size
+                .width, // MediaQuery.of(context)... proporciona datos sobre el tamaño de la pantalla, la orientación, la densidad de píxeles, los márgenes seguros (safe areas), la escala del texto y otros aspectos del diseño.
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(),
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                children: <Widget>[
+                  Text("Lasagna"),
+                  Text("Jorge U. Chavira"),
+                  Container(height: 1, width: 75, color: Colors.green),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
